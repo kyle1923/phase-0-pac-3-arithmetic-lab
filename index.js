2161d0c6f1bbb7a8dabc13ae76280e31d8ec46e7
Kyle Vermillion <kvermillion23@gmail.com>
2022-06-16 23:13:05 -0400

    commit tree ab88a6b9baa21ae3e5b87ad397072455a8569f83

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+let a = 0;
+function increment(a) {
+    return a + 1;
+} 
+
+let b = 0;
+function decrement(b) {
+    return b - 1;
+} 
+function makeInt(a) {
+    return parseInt(a, 10) ;
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a, 10);
+}
